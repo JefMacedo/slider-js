0b21932a21244dcc49d360414cd416bf71b12c88
Jeferson Macedo <jhef.salles@gmail.com>
2021-01-19 06:50:08 -0400

    correção de bug e add conteudo

--- script.js
+++ script.js
@@ -24,7 +24,10 @@ function goNext() {
 }
 
 function updateMargin() {
-    let newMargin = (currentSlide * document.body.clientWidth);
+    let sliderItemWidth = document.querySelector('.slider-item').clientWidth;
+    let newMargin = (currentSlide * sliderItemWidth);
     document.querySelector('.slider-width').style.marginLeft = 
         `-${newMargin}px`;
 }
+
+setInterval(goNext, 6500);
